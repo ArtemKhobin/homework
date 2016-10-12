d3ef7db23880678b326e0525f92a105d9516374d
Artem Khobin <artem.khobin@gmail.com>
2016-10-12 16:38:56 +0300

    removed unnecessary functional

--- lesson2/akhobin/src/task/Converter.java
+++ lesson2/akhobin/src/task/Converter.java
@@ -3,51 +3,23 @@ package task;
 class Converter {
   public static void main(String[] args) {
 
-    String str = "-1101010.011";
+    String str = "1011011";
 
     System.out.println( convert(str) );
   }
 
-  public static float convert(String binary) {
-    // get number sign
-    int sign = binary.charAt(0) == '-' ? -1 : 1;
-    binary = binary.replace("-", "");
+  public static int convert(String binary) {
+    int result = 0;
+    int length = binary.length();
 
-    // Get integer and fractional parts
-    String intPart, fractPart = "";
-    int dotPosition = binary.indexOf(".");
-    if ( dotPosition > -1 ) {
-      intPart = binary.substring(0, dotPosition);
-      fractPart = binary.substring(dotPosition + 1);
-    } else {
-      intPart = binary;
-    }
-
-    float result = 0;
-    // Convert the integer part
-    int length = intPart.length();
     for (int i = 0; i < length; i++) {
-      if ( intPart.charAt(i) == '1' ) {
+      if ( binary.charAt(i) == '1' ) {
         result += Math.pow(2, (length - 1) - i);
-      } else if ( intPart.charAt(i) != '0' ) {
+      } else if ( binary.charAt(i) != '0' ) {
         System.out.println("Incorrect number");
         return 0;
       }
     }
-
-    // Convert the fractional part
-    length = fractPart.length();
-    if ( length > 0 ) {
-      for (int i = 0; i < length; i++) {
-        if ( fractPart.charAt(i) == '1' ) {
-          result += Math.pow(2, (i + 1) * -1);
-        } else if ( fractPart.charAt(i) != '0' ) {
-          System.out.println("Incorrect number");
-          return 0;
-        }
-      }
-    }
-
-    return result * sign;
+    return result;
   }
 }
